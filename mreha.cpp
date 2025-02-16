c15602211e9f249b02ee0e440d57868c0618e67b
zzik <edin.bashich@gmail.com>
2025-02-16 12:37:11 +0100

    moglo bi se ovo jos urednije popisat, al ono

--- mreha.cpp
+++ mreha.cpp
@@ -43,14 +43,9 @@ bool imal(int prviArray[], int drugiArray[], int treciArray[], int brojKockica) 
             }
         }
     }
-    //cout << "prvi broj: " << prviBroj << endl;
-    //cout << "drugi broj: " << drugiBroj << endl;
     return prviBroj && drugiBroj;
 }
 
-void repositionArray(int oldArr[], int newArr[]) {
-
-}
 
 int main()
 {
@@ -60,37 +55,31 @@ int main()
     const int brojKockica = 6;
     int prviArray[brojKockica] = { 0 }, drugiArray[brojKockica] = { 0 }, treciArray[brojKockica] = { 0 };
 
-    fillArray(prviArray);
-    fillArray(drugiArray);
-    fillArray(treciArray);
+    fillArray(prviArray); fillArray(drugiArray); fillArray(treciArray);
 
-    cout << "Bacanje 1: ";
-    printArray(prviArray);
-
-    cout << "Bacanje 2: ";
-    printArray(drugiArray);
-
-    cout << "Bacanje 3: ";
-    printArray(treciArray);
+    cout << "Bacanje 1: "; printArray(prviArray);
+    cout << "Bacanje 2: "; printArray(drugiArray);
+    cout << "Bacanje 3: "; printArray(treciArray);
     
     while (!imal(prviArray, drugiArray, treciArray, brojKockica)) {
-        repositionArray(prviArray, drugiArray);
-        repositionArray(drugiArray, treciArray);
+        for (int i = 0; i < brojKockica; i++) {
+            *(prviArray + i) = *(drugiArray + i);
+            *(drugiArray + i) = *(treciArray + i);
+        }
+
         fillArray(treciArray);
         brojac++;
         cout << "Bacanje " << brojac << ": ";
         printArray(treciArray);
-        cout << endl;
         
         if (brojac == 420) break;
     }
     
 
-    cout << "Ukupan broj bacanja je: " << brojac << endl;
-    cout << "Zadnje bacene kockice su: " << endl;
-    printArray(prviArray);
-    printArray(drugiArray);
-    printArray(treciArray);
+    cout << endl; cout << "Ukupan broj bacanja je: " << brojac << endl;
+    cout << endl; cout << "Zadnje bacene kockice su: " << endl;
+
+    printArray(prviArray); printArray(drugiArray); printArray(treciArray);
 
     return 0;
 }
